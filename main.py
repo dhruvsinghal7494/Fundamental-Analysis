6499d9cce9c1be84d3b45885c688f722c8d2d2f6
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-10 12:21:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 print("Hello world")
 
-sector
-market cap
-
-P/E
-median P/E
-sector P/E
-Promotor holding and change
-public holding and change
-FII, DII and change
-
-ROCE and ROE
-sales growth 10 years
-profit growth 10 years
-borrowings
-trade receivables
-
 market_cap = int(input('Enter the marketcap: '))
-if marketcap less than 5000cr, then penalty
+#if marketcap less than 5000cr, then penalty
 
 sector = input('Select your sector: ')
 IT
@@ -31,15 +15,25 @@ AUTO
 PSU
 Others
 
+pe = 
+median_pe = []
+sector_pe = 
 
+sales = []
+sales_growth = []
+profit = []
+sales_growth = []
 
+promotor_holding = []
+institutional_holding = []
+public_holding = []
 
+roc = [] #Not for banks
+roce = []
 
-
-
-
-
-
+borrowings = []
+interest_payment = []
+trade_receivables = 
 
 
 
