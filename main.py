88cc66d66271ac512e70cb941a1855fd0cb19336
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-10 11:53:35 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+print("Hello world")
+
+sector
+market cap
+
+P/E
+median P/E
+sector P/E
+Promotor holding and change
+public holding and change
+FII, DII and change
+
+ROCE and ROE
+sales growth 10 years
+profit growth 10 years
+borrowings
+trade receivables
+
+market_cap = int(input('Enter the marketcap: '))
+if marketcap less than 5000cr, then penalty
+
+sector = input('Select your sector: ')
+IT
+FMCG
+BANKING
+FINANCE
+PHARMA
+INFRA
+COMMODITY
+AUTO
+PSU
+Others
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
