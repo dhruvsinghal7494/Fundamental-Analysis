dbc6f222416d143c98c7c368083ec44e86ebea2a
dhruvsinghal7494 <122340013+dhruvsinghal7494@users.noreply.github.com>
2023-01-11 16:11:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import streamlit as st
 #pe = 
 #median_pe = #10year 
 #sector_pe = 
-#pe_points = 15*(0.67(median_pe/pe) + 0.33(sector_pe/pe))
+#pe_points = 15*(0.67(median_pe-pe) + 0.33(sector_pe-pe))
 
 #30%
 #sales = [] #year 
@@ -27,31 +27,33 @@ import streamlit as st
 #profit = []
 #profit_growth = [] #atleast 3 times
 #ebita = []
-#sales_points = 30*(())
 
+sales_points = 7*((sales-sales2)/sales2)
+if(sales_points>=30)
+   {sales_points=30}
 
 #20%
 #promotor_holding = [] #year and >50% except banking
 #institutional_holding = []
 #public_holding = []
-#holding_points1 = 15*(0.55*(promotor_holding) + 0.3*(institutional_holding) + 0.15*(public_holding))
-#holding_points2 = 5*(100*(promotor_holding_change) + 100*(institutional_holding_change) - 100*(public_holding_change))
+holding_points1 = 15*(0.55*(promotor_holding) + 0.3*(institutional_holding) + 0.15*(public_holding))
+holding_points2 = 5*(100*(promotor_holding_change) + 100*(institutional_holding_change) - 100*(public_holding_change))
 
 #20%
 #roc = [] #Not for banks, min 15 
 #roce = [] 
-#roc_points = 20*((roc*0.45 + roce*0.55 - 15))
+roc_points = 20*((roc*0.45 + roce*0.55 - 15))
 
 #15%
 #borrowings = [] #must be less than 10% of marketcap
 #interest_payment = [] 
-#trade_receivables = []
+#9trade_receivables = []
 
 
 pe = st.number_input("Enter the current P/E: ")
 median_pe = st.number_input("Enter the median P/E of last 10 years: ")
 sector_pe = st.number_input("Enter the current sector P/E: ")
-pe_points = 15*((0.67*(median_pe/pe)) + 0.33*(sector_pe/pe))
+pe_points = 15*(0.67(median_pe-pe) + 0.33(sector_pe-pe))
 
 market_cap = st.number_input("Enter the current marketcap: ")
 sector = st.number_input("Enter the sector: ")
